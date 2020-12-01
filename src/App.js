fb18e8176c1878467c24654d0388bbf79ea4b92c
Nabajyoti <sirnaba@gmail.com>
2020-12-01 21:28:52 +0530

    adding files for deployment

--- src/App.js
+++ src/App.js
@@ -29,18 +29,23 @@ class App extends Component{
       })
   }
 
+  handleChange = (e) => {
+    this.setState({searchField : e.target.value})
+  }
+
   render(){
     const {monsters, searchField } = this.state;
-    
+
     const filteredMonsters = monsters.filter(monster => 
       monster.name.toLowerCase().includes(searchField.toLowerCase())
       )
 
     return(
           <div className="App">
+            <h1>Monster Rolodex</h1>
             <SearchBox 
             placeholder="Search monsters"
-            handleChange={e => this.setState({searchField : e.target.value})}>
+            handleChange={this.handleChange}>
             </SearchBox>
 
             <CardList monsters={filteredMonsters} >
